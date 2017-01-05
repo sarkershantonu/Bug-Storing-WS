03b1a4c99f8f903819ab9464cbef05278ae963aa
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-04 23:13:27 -0500

    property reference added

--- src/main/java/org/automation/bug/ws/WebSecurityConfig.java
+++ src/main/java/org/automation/bug/ws/WebSecurityConfig.java
@@ -1,7 +1,32 @@
 package org.automation.bug.ws;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * Created by shantonu on 1/4/17.
+ * read property ref : http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html
  */
-public class WebSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    @Value("${security.user.name}")
+    private String user;
+    @Value("${security.user.password}")
+    private String pass;
+
+    @Override
+    public void configure(HttpSecurity http){
+
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("").password("").roles("");
+    }
+
 }
