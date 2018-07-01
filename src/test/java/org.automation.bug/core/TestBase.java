fcd02c1ef6ff302352d4ec499f8006da78cd4a0e
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-01 01:12:14 -0400

    base test

--- src/test/java/org.automation.bug/core/TestBase.java
+++ src/test/java/org.automation.bug/core/TestBase.java
@@ -1,7 +1,14 @@
 package org.automation.bug.core;
 
+import org.automation.bug.ws.App;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by SSarker on 7/1/2018.
  */
-public class TestBase {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+public abstract class TestBase {
 }
