e4ab4a7ca51902eb4bdc53faacdf0ce0303f72d1
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-01 01:12:20 -0400

    regression suit

--- src/test/java/org.automation.bug/RegressionTests.java
+++ src/test/java/org.automation.bug/RegressionTests.java
@@ -1,7 +1,14 @@
 package org.automation.bug;
 
+import org.automation.bug.service.BugServiceBeanTest;
+import org.automation.bug.web.api.BugControllerTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 /**
  * Created by SSarker on 7/1/2018.
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BugControllerTests.class, BugServiceBeanTest.class})
 public class RegressionTests {
 }
