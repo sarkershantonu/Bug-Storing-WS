d0754fd2d82fb33150a19a8826ad040667d68761
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-02 02:55:46 -0400

    spring test

--- src/test/java/org.automation.bug/service/BugServiceBeanTest.java
+++ src/test/java/org.automation.bug/service/BugServiceBeanTest.java
@@ -1,7 +1,9 @@
 package org.automation.bug.service;
 
+import org.automation.bug.core.SpringTestBase;
+
 /**
  * Created by SSarker on 7/1/2018.
  */
-public class BugServiceBeanTest {
+public class BugServiceBeanTest extends SpringTestBase{
 }
