f8a162dc7b87bad1cf9a06fdfd89aa5306d121a1
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-02 04:27:26 -0400

    dummy bug commonly added

--- src/test/java/org.automation.bug/core/SpringTestBase.java
+++ src/test/java/org.automation.bug/core/SpringTestBase.java
@@ -1,6 +1,7 @@
 package org.automation.bug.core;
 
 import org.automation.bug.ws.App;
+import org.automation.bug.ws.model.Bug;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -11,4 +12,20 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 public abstract class SpringTestBase {
+
+    protected Bug getADummyBug(){
+        Bug aBug = new Bug();
+        aBug.setId(null);
+        aBug.setTitle("Title");
+        aBug.setSummary("summary");
+        aBug.setDescription("Description");
+        aBug.setAttachmentPath("AttachmentPath");
+        aBug.setExceptions("exceptions");
+        aBug.setComments("comments");
+        aBug.setFoundDateTime("foundDateTime");
+        aBug.setTags("TAG1,TAG2");
+        aBug.setPriority("5");
+        aBug.setServility("CRITICAL");
+        return aBug;
+    }
 }
