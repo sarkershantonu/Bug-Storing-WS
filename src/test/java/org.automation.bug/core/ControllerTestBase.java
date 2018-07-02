924ee85f0a1422bf49f753b24107765538e9ef15
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-02 02:55:39 -0400

    controller with utils

--- src/test/java/org.automation.bug/core/ControllerTestBase.java
+++ src/test/java/org.automation.bug/core/ControllerTestBase.java
@@ -1,5 +1,7 @@
 package org.automation.bug.core;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.automation.bug.ws.web.api.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -7,11 +9,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by SSarker on 7/1/2018.
  */
 @WebAppConfiguration
-public abstract class ControllerTestBase extends TestBase {
+public abstract class ControllerTestBase extends SpringTestBase {
     protected MockMvc mockMvc;
 
     @Autowired
@@ -26,4 +30,14 @@ public abstract class ControllerTestBase extends TestBase {
    /* protected <T extends BaseController> void init(T controller){
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }*/
+   //utils for JSON parsing
+   protected String toJson(Object object) throws JsonProcessingException {
+       ObjectMapper mapper = new ObjectMapper();
+       return mapper.writeValueAsString(object);
+   }
+    //utils for JSON parsing 
+  protected <T> T parsefrom(String jsonData, Class<T> klass) throws IOException {
+       ObjectMapper mapper = new ObjectMapper();
+       return mapper.readValue(jsonData,klass);
+  }
 }
