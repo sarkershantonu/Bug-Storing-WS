666cd19b7e01645f33f080411145284b09173986
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-06 03:42:44 -0400

    update

--- src/test/java/org.automation.bug/core/ServiceTestBase.java
+++ src/test/java/org.automation.bug/core/ServiceTestBase.java
@@ -1,7 +1,12 @@
 package org.automation.bug.core;
 
+import org.automation.bug.ws.model.Bug;
+import org.mockito.ArgumentCaptor;
+
 /**
  * Created by SSarker on 7/6/2018.
  */
-public class ServiceTestBase {
+public class ServiceTestBase extends SpringTestBase {
+
+    protected ArgumentCaptor<Bug> argumentCaptorForBug = ArgumentCaptor.forClass(Bug.class);
 }
