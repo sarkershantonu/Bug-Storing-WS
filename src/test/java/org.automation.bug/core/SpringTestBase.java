841eb6498a65fc01ef13ad36a28495d0bf676590
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-03 23:44:04 +0600

    removed test config

--- src/test/java/org.automation.bug/core/SpringTestBase.java
+++ src/test/java/org.automation.bug/core/SpringTestBase.java
@@ -8,7 +8,6 @@ import org.automation.bug.ws.model.Bug;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
