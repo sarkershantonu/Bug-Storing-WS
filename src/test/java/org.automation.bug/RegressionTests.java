5ee16715c5686c9dbe29e1024dbc7d843196b6f4
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-05 00:24:50 +0600

    junit 5 migration

--- src/test/java/org.automation.bug/RegressionTests.java
+++ src/test/java/org.automation.bug/RegressionTests.java
@@ -1,6 +1,6 @@
 package org.automation.bug;
 
-import org.automation.bug.service.BugServiceBeanTest;
+import org.automation.bug.service.BugServiceTests;
 import org.automation.bug.web.api.BugControllerTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,6 +9,6 @@ import org.junit.runners.Suite;
  * Created by SSarker on 7/1/2018.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({BugControllerTests.class, BugServiceBeanTest.class})
+@Suite.SuiteClasses({BugControllerTests.class, BugServiceTests.class})
 public class RegressionTests {
 }
