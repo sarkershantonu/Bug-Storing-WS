a0e2566945b14dba1ac7b517ee45932a9672e636
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-05 00:17:50 +0600

    junit 5 migration

--- src/test/java/org.automation.bug/web/api/BugControllerTests.java
+++ src/test/java/org.automation.bug/web/api/BugControllerTests.java
@@ -95,7 +95,7 @@ public class BugControllerTests extends ControllerTestBase {
         Assertions.assertEquals(Integer.valueOf(HttpStatus.ACCEPTED.toString()).intValue(),result.getResponse().getStatus());
         Bug resultBug = super.parse(result.getResponse().getContentAsString(),Bug.class);
         Assertions.assertEquals(aBugToUpdate,resultBug);
-        Assertions.assertTrue("Not matched data",aBugToUpdate.equalsByData(resultBug));
+        Assertions.assertTrue(aBugToUpdate.equalsByData(resultBug));
     }
 
     @Test
