5c3ad7b4cd79bc8da3581881c72d94ec766499f9
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-07 23:57:12 +0600

    bean config

--- src/main/java/org/automation/bug/ws/config/WebSecurityConfig.java
+++ src/main/java/org/automation/bug/ws/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package org.automation.bug.ws;
+package org.automation.bug.ws.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
