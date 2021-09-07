23c6123d6611c1544b61a7d00a2a91464bd9330e
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-07 23:53:51 +0600

    cache config

--- src/main/java/org/automation/bug/ws/config/GuavaCache.java
+++ src/main/java/org/automation/bug/ws/config/GuavaCache.java
@@ -1,4 +1,23 @@
 package org.automation.bug.ws.config;
 
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+
 public class GuavaCache {
+    @Bean
+    public CacheManager cacheManager(){
+//GuavaCacheManager removed in spring boot 2
+        return new GuavaCacheManager("bugs");
+    }
+    public CacheManager cacheManagerExplicit(){
+
+        GuavaCacheManager cache  = new GuavaCacheManager("bugs");
+        CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(500).expireAfterWrite(30, TimeUnit.SECONDS);
+        cache.setCacheBuilder(builder);
+        return cache;
+    }
 }
