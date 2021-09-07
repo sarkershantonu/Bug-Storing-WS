d6f34268b257b6bbe0ec2b8a31ff39a9cfe7c774
shantonu sarker <sarker.shantonu@gmail.com>
2021-09-08 00:38:23 +0600

    exception disabled

--- src/main/java/org/automation/bug/ws/web/api/BaseController.java
+++ src/main/java/org/automation/bug/ws/web/api/BaseController.java
@@ -1,10 +1,11 @@
 package org.automation.bug.ws.web.api;
 
+import org.automation.bug.ws.exception.DefaultExceptionAttributes;
+import org.automation.bug.ws.exception.ExceptionsAttributes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,7 @@ import java.util.Map;
 public class BaseController {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @ExceptionHandler(Exception.class)
+  /*  @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> manageException(Exception e, HttpServletRequest request){
         logger.error("Handling exception.... \nException",e);
         ExceptionsAttributes attributes = new DefaultExceptionAttributes();
@@ -29,5 +30,5 @@ public class BaseController {
         logger.error("Handling Null response.... \nException",e);
         Map<String,Object> response = new DefaultExceptionAttributes().getExceptionAttributes(e, request,HttpStatus.NOT_FOUND);
         return new ResponseEntity(response,HttpStatus.NOT_FOUND);
-    }
+    }*/
 }
