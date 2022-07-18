83956ab0e1b8cfffc996e92d006ab8e428cb24dd
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-17 23:58:24 -0500

    refactor : test moved

--- bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
@@ -1,6 +1,6 @@
-package org.automation.bug.core;
+package org.automation.bug.ws.core;
 
-import org.automation.bug.ws.model.Bug;
+import org.automation.bug.ws.ws.model.Bug;
 import org.mockito.ArgumentCaptor;
 
 /**
