ed8011b6782e6c9d4c5e5e90e7fc1b68b18e4100
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-17 23:58:20 -0500

    refactor : test moved

--- bug-store/src/test/java/org/automation/bug/ws/core/SpringTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/SpringTestBase.java
@@ -1,10 +1,10 @@
-package org.automation.bug.core;
+package org.automation.bug.ws.core;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.automation.bug.ws.App;
-import org.automation.bug.ws.model.Bug;
+import org.automation.bug.ws.ws.App;
+import org.automation.bug.ws.ws.model.Bug;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 import java.util.List;
