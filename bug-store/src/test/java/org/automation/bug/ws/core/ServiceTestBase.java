d32b0df57c27a5cb1a304b939e5d0144c258c89b
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-22 00:03:05 -0500

    Test type added

--- bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
@@ -6,7 +6,7 @@ import org.mockito.ArgumentCaptor;
 /**
  * Created by SSarker on 7/6/2018.
  */
-public class ServiceTestBase extends SpringTestBase {
+public abstract class ServiceTestBase extends SpringTestBase {
 
     protected ArgumentCaptor<Bug> argumentCaptorForBug = ArgumentCaptor.forClass(Bug.class);
 }
