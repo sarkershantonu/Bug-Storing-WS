c49fc8d804bf41ac0a272bdd45e803d4a83d6d32
shantonu sarker <sarker.shantonu@gmail.com>
2025-06-08 08:29:50 -0500

    todo : replacing WebSecurityConfigurerAdapter

--- bug-store/src/main/java/org/automation/bug/ws/ws/config/WebSecurityConfig.java
+++ bug-store/src/main/java/org/automation/bug/ws/ws/config/WebSecurityConfig.java
@@ -1,13 +1,8 @@
 package org.automation.bug.ws.ws.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by shantonu on 1/4/17.
