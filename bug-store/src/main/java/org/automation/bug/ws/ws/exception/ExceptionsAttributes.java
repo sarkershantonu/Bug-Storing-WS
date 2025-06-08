79f2c80e270c4481cc1a132d493f07ced4d813a6
shantonu sarker <sarker.shantonu@gmail.com>
2025-06-08 08:29:19 -0500

    updating => spring and others

--- bug-store/src/main/java/org/automation/bug/ws/ws/exception/ExceptionsAttributes.java
+++ bug-store/src/main/java/org/automation/bug/ws/ws/exception/ExceptionsAttributes.java
@@ -1,8 +1,8 @@
 package org.automation.bug.ws.ws.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
