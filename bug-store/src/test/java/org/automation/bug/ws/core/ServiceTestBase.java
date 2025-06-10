96770906deb96aa0fb8c3f648556399b74e2e28d
shantonu sarker <sarker.shantonu@gmail.com>
2025-06-10 01:20:05 -0500

    security updated, need to fix junit 5 runner

--- bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/ServiceTestBase.java
@@ -6,7 +6,7 @@ import org.mockito.ArgumentCaptor;
 /**
  * Created by SSarker on 7/6/2018.
  */
-public abstract class ServiceTestBase extends SpringTestBase {
+public abstract class ServiceTestBase extends BugWsTestBase {
 
     protected ArgumentCaptor<Bug> argumentCaptorForBug = ArgumentCaptor.forClass(Bug.class);
 }
