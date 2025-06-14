278e8b7056dbb346004d8d94c6b81cec0aad35ef
shantonu sarker <sarker.shantonu@gmail.com>
2025-06-14 01:44:39 -0500

    Test Error Fix, todo need to fix test cases for validations

--- bug-store/src/test/java/org/automation/bug/ws/suites/RegressionTestSuite.java
+++ bug-store/src/test/java/org/automation/bug/ws/suites/RegressionTestSuite.java
@@ -2,16 +2,16 @@ package org.automation.bug.ws.suites;
 
 import org.automation.bug.ws.tests.services.BugServiceTests;
 import org.automation.bug.ws.tests.apis.BugControllerTests;
-import org.junit.jupiter.api.Disabled;
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
 
 /**
  * Created by SSarker on 7/1/2018.
  */
-@RunWith(JUnitPlatform.class)
+@Suite
+@SuiteDisplayName("Regression Test")
 @SelectClasses({BugControllerTests.class, BugServiceTests.class})
-//@Disabled
 public class RegressionTestSuite {
 }
