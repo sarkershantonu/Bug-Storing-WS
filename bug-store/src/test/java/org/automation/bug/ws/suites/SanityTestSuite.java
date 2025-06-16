7fde06c3d30f44f41acd369288c55ca73ccf9955
shantonu sarker <sarker.shantonu@gmail.com>
2025-06-16 07:43:35 -0500

    new suite added => todo, fixing DB error

--- bug-store/src/test/java/org/automation/bug/ws/suites/SanityTestSuite.java
+++ bug-store/src/test/java/org/automation/bug/ws/suites/SanityTestSuite.java
@@ -1,4 +1,13 @@
 package org.automation.bug.ws.suites;
 
+import org.automation.bug.ws.tests.apis.BugControllerTests;
+import org.automation.bug.ws.tests.services.BugServiceTests;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Regression Test")
+@SelectClasses({BugControllerTests.class, BugServiceTests.class})
 public class SanityTestSuite {
 }
