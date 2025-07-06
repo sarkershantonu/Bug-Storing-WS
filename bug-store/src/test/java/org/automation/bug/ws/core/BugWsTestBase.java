b0b25598b49f967eb0270ac1d447636b2ee79f3a
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-06 01:09:46 -0500

    new CSRF test case + organize auth tests + lang3 added

--- bug-store/src/test/java/org/automation/bug/ws/core/BugWsTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/BugWsTestBase.java
@@ -3,18 +3,34 @@ package org.automation.bug.ws.core;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Filter;
 import org.automation.bug.ws.ws.BugWsApp;
 import org.automation.bug.ws.ws.model.Bug;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 /**
  * Created by SSarker on 7/1/2018.
  */
 @SpringBootTest(classes = BugWsApp.class)
 public abstract class BugWsTestBase {
+
+    protected RequestPostProcessor anUser(){
+        return user("shantonu").password("123456");
+    }
+    protected RequestPostProcessor testUser(){
+        return user("shantonu").password("123456").roles("USER");
+    }
+    protected RequestPostProcessor testAdmin(){
+        return user("shantonu").password("123456").roles("ADMIN");
+    }
     protected String toJson(Object object) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(object);
