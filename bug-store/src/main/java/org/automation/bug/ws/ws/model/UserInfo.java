edd011e4e6023b9b6d603e36472e9df3bb587bb9
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-06 01:09:53 -0500

    new CSRF test case + organize auth tests + lang3 added

--- bug-store/src/main/java/org/automation/bug/ws/ws/model/UserInfo.java
+++ bug-store/src/main/java/org/automation/bug/ws/ws/model/UserInfo.java
@@ -1,4 +1,57 @@
 package org.automation.bug.ws.ws.model;
 
-public class UserInfo {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserInfo implements Serializable {
+    private long id;
+    private String name;
+
+    public UserInfo() {
+        super();
+    }
+
+    public UserInfo(final String name) {
+        super();
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return id == userInfo.id && Objects.equals(name, userInfo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Foo [name=")
+                .append(name)
+                .append("]");
+        return builder.toString();
+    }
 }
