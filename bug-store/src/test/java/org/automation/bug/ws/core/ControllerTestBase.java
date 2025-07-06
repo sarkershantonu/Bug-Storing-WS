640fd727d84bf0d8b07cdab9f6fb02717e0298d2
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-06 01:09:51 -0500

    new CSRF test case + organize auth tests + lang3 added

--- bug-store/src/test/java/org/automation/bug/ws/core/ControllerTestBase.java
+++ bug-store/src/test/java/org/automation/bug/ws/core/ControllerTestBase.java
@@ -1,5 +1,6 @@
 package org.automation.bug.ws.core;
 
+import jakarta.servlet.Filter;
 import org.automation.bug.ws.ws.web.api.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.web.context.WebApplicationContext;
 @WebAppConfiguration
 public abstract class ControllerTestBase extends BugWsTestBase {
     protected MockMvc mockMvc;
+
 
     @Autowired
     protected WebApplicationContext context;
