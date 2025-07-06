30ba84cdd62030db44156ae68635a7e4fdd4572e
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-05 23:59:42 -0500

    new CSRF test case

--- bug-store/src/test/java/org/automation/bug/ws/tests/config/CsrfTests.java
+++ bug-store/src/test/java/org/automation/bug/ws/tests/config/CsrfTests.java
@@ -3,4 +3,7 @@ package org.automation.bug.ws.tests.config;
 import org.automation.bug.ws.core.BugWsTestBase;
 
 public class CsrfTests extends BugWsTestBase {
+    public void CsrfEnableTest(){}
+
+    public void CsrfDesableTest(){}
 }
