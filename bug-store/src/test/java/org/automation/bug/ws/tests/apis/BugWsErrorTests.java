3d3dd91a2929a85eebae6fa3d4f1ceec6d221516
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-17 00:01:40 -0500

    test for creating negative scenarios

--- bug-store/src/test/java/org/automation/bug/ws/tests/apis/BugWsErrorTests.java
+++ bug-store/src/test/java/org/automation/bug/ws/tests/apis/BugWsErrorTests.java
@@ -1,9 +1,15 @@
 package org.automation.bug.ws.tests.apis;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class BugWsErrorTests extends BugControllerTests{
     public void testMethodTraceDisabled(){}
     public void testMethodHeadDisabled(){}
     public void testMethodOptionsDisabled(){}
-    public void testMethodDisabled(){}
+    public void testMethodPatchDisabled(){}
     public void testMethodConnectDisabled(){}
+
+    public void testMethodDisabled(final RequestMethod methodName, final String url){
+
+    }
 }
