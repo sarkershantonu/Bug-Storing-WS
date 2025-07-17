397cc31370d1030a4ac0a9aeb5b1aea5d04c2191
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-17 00:08:42 -0500

    test for creating negative scenarios

--- bug-store/src/test/java/org/automation/bug/ws/tests/apis/BugWsErrorTests.java
+++ bug-store/src/test/java/org/automation/bug/ws/tests/apis/BugWsErrorTests.java
@@ -1,15 +1,19 @@
 package org.automation.bug.ws.tests.apis;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public class BugWsErrorTests extends BugControllerTests{
-    public void testMethodTraceDisabled(){}
-    public void testMethodHeadDisabled(){}
-    public void testMethodOptionsDisabled(){}
-    public void testMethodPatchDisabled(){}
-    public void testMethodConnectDisabled(){}
+    private int expectedNotImplemented = HttpStatus.NOT_IMPLEMENTED.value();
+    @Test public void testMethodTraceDisabled(){}
+    @Test public void testMethodHeadDisabled(){}
+    @Test public void testMethodOptionsDisabled(){}
+    @Test public void testMethodPatchDisabled(){}
+    @Test public void testMethodConnectDisabled(){}
 
     public void testMethodDisabled(final RequestMethod methodName, final String url){
-
+        Assertions.assertEquals(expectedNotImplemented,0);
     }
 }
