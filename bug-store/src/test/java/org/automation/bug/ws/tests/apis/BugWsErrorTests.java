a30f11956b01c04a803134b562110328b15c730c
shantonu sarker <sarker.shantonu@gmail.com>
2025-07-16 23:57:51 -0500

    test for creating negative scenarios

---
+++ bug-store/src/test/java/org/automation/bug/ws/tests/apis/BugWsErrorTests.java
@@ -0,0 +1,9 @@
+package org.automation.bug.ws.tests.apis;
+
+public class BugWsErrorTests extends BugControllerTests{
+    public void testMethodTraceDisabled(){}
+    public void testMethodHeadDisabled(){}
+    public void testMethodOptionsDisabled(){}
+    public void testMethodDisabled(){}
+    public void testMethodConnectDisabled(){}
+}
