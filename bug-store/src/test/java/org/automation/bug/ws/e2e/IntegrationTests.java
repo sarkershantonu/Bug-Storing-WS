96eb554c8e62454c3933a364ade52af798bc608d
shantonu sarker <sarker.shantonu@gmail.com>
2025-08-19 23:52:53 -0500

    planning for End to end test adding

--- bug-store/src/test/java/org/automation/bug/ws/e2e/IntegrationTests.java
+++ bug-store/src/test/java/org/automation/bug/ws/e2e/IntegrationTests.java
@@ -1,4 +1,14 @@
 package org.automation.bug.ws.e2e;
 
-public class IntegrationTests {
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class IntegrationTests {
+
+    @BeforeAll
+    public static void initTest(){}
+    private static void configRestAssured(){
+        RestAssured.baseURI="http://localhost:9100";
+
+    }
 }
